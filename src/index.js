0aad1936062865011cf9487eb04c1a3214afa4f3
Karol-Szykula <karol.szykula@wp.pl>
2018-12-08 14:29:45 +0100

    Add mui theme provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import './index.css'
 import App from './App'
 
 
 ReactDOM.render(
-    <App />,
+    <Provider>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Provider>,
     document.getElementById('root')
 )
